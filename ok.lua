93ec08ccd296935b06a5f3f3534b19115ba46d3c
rblxrens <104765464+rblxrens@users.noreply.github.com>
2022-05-06 21:24:37 +0100

    Update ok.lua

--- ok.lua
+++ ok.lua
@@ -1,18 +1,11 @@
-	for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
-		v:Disable()
+_G.Thing = true
+_G.Case = "Darklaced"
 
-
-		for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
-			v:Disable()
-			_G.Thing = true
-			_G.Case = "Darklaced"
-
-			for i=1,1400 do
-				spawn(function()
-					while _G.Thing do 
-						game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(_G.Case, tostring(game:GetService("Players").LocalPlayer.CasesPer.Value))
-						game:GetService("ReplicatedStorage").Remotes.Click:FireServer()
-					end
-				end)
-			end
-		end)
+for i=1,700 do
+spawn(function()
+    while _G.Thing do 
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(_G.Case, tostring(game:GetService("Players").LocalPlayer.CasesPer.Value))
+        game:GetService("ReplicatedStorage").Remotes.Click:FireServer()
+    end
+end)
+end
