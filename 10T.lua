45b9f0b698c92cbff32c584c2234c03d644af695
rblxrens <104765464+rblxrens@users.noreply.github.com>
2022-05-07 09:10:15 +0100

    Create 10T.lua

---
+++ 10T.lua
@@ -0,0 +1,22 @@
+	local string_1 = "Dark Ethereal Hairstyle";
+	local string_2 = "5000";
+	local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+	Target:FireServer(string_1, string_2);
+
+
+	local string_1 = "Dark Cat Mask";
+	local string_2 = "5000";
+	local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+	Target:FireServer(string_1, string_2);
+
+
+	local string_1 = "Dark Devil Hood";
+	local string_2 = "5000";
+	local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+	Target:FireServer(string_1, string_2);
+
+
+	local string_1 = "Dark Ushanka";
+	local string_2 = "5000";
+	local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+	Target:FireServer(string_1, string_2);
