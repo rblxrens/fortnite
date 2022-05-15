e926007d2c00777024308d83f44871560cb2f113
rblxrens <104765464+rblxrens@users.noreply.github.com>
2022-05-15 15:35:34 +0100

    Update case.lua

--- case.lua
+++ case.lua
@@ -2,5 +2,5 @@ _G.autocase = true
 while _G.autocase == true do
     local args = {[1] = "Starter"}
     game:GetService("ReplicatedStorage").Events.OpenCase:InvokeServer(unpack(args))
-    wait(0.30)
+    wait(0.01)
     end
